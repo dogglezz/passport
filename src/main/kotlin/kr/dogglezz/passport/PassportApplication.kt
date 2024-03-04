ccc8b05c6659434547e698f72e469c0c8fa644ec
Chanbok Wi <cksqhr4961@gmail.com>
2024-03-04 20:10:32 +0900

    create project

---
+++ src/main/kotlin/kr/dogglezz/passport/PassportApplication.kt
@@ -0,0 +1,11 @@
+package kr.dogglezz.passport
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PassportApplication
+
+fun main(args: Array<String>) {
+	runApplication<PassportApplication>(*args)
+}
